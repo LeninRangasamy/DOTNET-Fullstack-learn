9559dcf627b012ca834b744b1b8bfff7fe1c2078
Lenin <leninlenin077@gmail.com>
2025-11-19 03:38:02 +0530

    Employee model class

---
+++ DOTNET_FS/ASP.NET core MVC/Week 2/CRUD(in-memory)/Models/EmployeeMoodel.cs
@@ -0,0 +1,7 @@
+namespace ASPEmpty.Models{
+    public class EmployeeModel{
+      public int eid {get; set;}
+      public string name {get; set;}=string.Empty;
+      public double salary {get; set;}
+    }
+}
