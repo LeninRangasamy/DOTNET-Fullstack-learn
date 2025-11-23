e67b603eff4fd7a916f34735d36c910f397626a3
Lenin <leninlenin077@gmail.com>
2025-11-23 20:50:38 +0530

    Add files via upload

---
+++ DOTNET_FS/C#/Week_4/Models/StudentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace StudentManagement.Models.StudentModel;
+
+public class Student
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public int marks { get; set; }
+}
